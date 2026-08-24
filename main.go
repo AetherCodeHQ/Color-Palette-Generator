54cf5e221d84f65234d00594b4e12ec9c0281e03
AetherCodeHQ <noreply@users.noreply.github.com>
2026-08-24 15:46:17 +0300

    v2.1.0: unique implementation replaces generic stub

--- main.go
+++ main.go
@@ -1,40 +1,65 @@
-
 package main
 
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("usage: Color-Palette-Generator <bytes|lines|reverse|upper|lower>")
-		os.Exit(1)
-	}
-	mode := os.Args[1]
-	in := ""
-	if len(os.Args) > 2 {
-		in = os.Args[2]
-	}
-	lines := strings.Split(in, "\n")
-	switch mode {
-	case "lines":
-		fmt.Println(len(lines))
-	case "bytes":
-		fmt.Println(len(in))
-	case "upper":
-		fmt.Println(strings.ToUpper(in))
-	case "lower":
-		fmt.Println(strings.ToLower(in))
-	case "reverse":
-		runes := []rune(in)
-		for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-			runes[i], runes[j] = runes[j], runes[i]
+	base := 200.0
+	if len(os.Args) > 1 {
+		if v, err := strconv.ParseFloat(os.Args[1], 64); err == nil {
+			base = v
 		}
-		fmt.Println(string(runes))
-	default:
-		fmt.Fprintln(os.Stderr, "unknown mode:", mode)
-		os.Exit(1)
 	}
+	var b strings.Builder
+	b.WriteString("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"500\" height=\"100\">\n")
+	for i := 0; i < 5; i++ {
+		h := base + float64(i)*30
+		if h >= 360 {
+			h -= 360
+		}
+		r, g, blue := hslToRgb(h, 0.7, 0.5)
+		fmt.Fprintf(&b, "<rect x=\"%d\" y=\"0\" width=\"100\" height=\"100\" fill=\"rgb(%d,%d,%d)\"/>\n", i*100, r, g, blue)
+	}
+	b.WriteString("</svg>\n")
+	fmt.Print(b.String())
+}
+
+func hslToRgb(h, s, l float64) (int, int, int) {
+	c := (1.0 - abs(2.0*l-1.0)) * s
+	x := c * (1.0 - abs(fmod(h/60.0, 2.0)-1.0))
+	m := l - c/2.0
+	var r, g, b float64
+	switch {
+	case h < 60:
+		r, g, b = c, x, 0
+	case h < 120:
+		r, g, b = x, c, 0
+	case h < 180:
+		r, g, b = 0, c, x
+	case h < 240:
+		r, g, b = 0, x, c
+	case h < 300:
+		r, g, b = x, 0, c
+	default:
+		r, g, b = c, 0, x
+	}
+	return int((r+m)*255 + 0.5), int((g+m)*255 + 0.5), int((b+m)*255 + 0.5)
+}
+
+func abs(f float64) float64 {
+	if f < 0 {
+		return -f
+	}
+	return f
+}
+
+func fmod(a, b float64) float64 {
+	for a >= b {
+		a -= b
+	}
+	return a
 }
